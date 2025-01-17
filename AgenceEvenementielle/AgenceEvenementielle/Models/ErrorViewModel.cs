f68cf290b559c6ed9b7fbd16beb195d9a2aeba24
Juliette117 <ajuliette7@gmail.com>
2025-01-17 09:51:13 +0100

    initialisation projet agence

---
+++ AgenceEvenementielle/AgenceEvenementielle/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgenceEvenementielle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
